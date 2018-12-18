68bcb0a78fa86e5cfc80e033e1f53657efb167a9
trabelsi radhwen <trabelsi.radhouen@gmail.com>
2018-12-18 12:41:13 +0100

    created update and delete asset's services

--- app/src/main/java/com/example/radhwen/snipeit/services/AssetServices.java
+++ app/src/main/java/com/example/radhwen/snipeit/services/AssetServices.java
@@ -7,9 +7,14 @@ import com.example.radhwen.snipeit.model.Rows;
 import com.example.radhwen.snipeit.model.StatusLabelRows;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface AssetServices {
@@ -23,4 +28,14 @@ public interface AssetServices {
                            @Query("name") ModelRows model,
                            @Query("name") CompanieRows company,
                            @Query("name") StatusLabelRows status);
+
+    @FormUrlEncoded
+    @PUT("hardware/{id}")
+    Call<Rows> updateAsset(@Header("Authorization") String api_key,
+                                     @Path("id") int id,
+                                     @Field("name") String name);
+
+    @DELETE("hardware/{id}")
+    Call<Rows> deleteAsset(@Header("Authorization") String api_key,
+                                     @Path("id") int id);
 }
